8e96057f29978fc640fc1976f9126cbe641a7a98
Gabriel Engel <gfbengel@gmail.com>
2023-04-23 12:14:31 -0300

    search gyms

---
+++ src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,72 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
+import { InMemoryGymsRepository } from '@/repositories/in-memory/gyms.repository'
+import { SearchGymsUseCase } from './search-gyms.use-case'
+
+let gymsRepository: InMemoryGymsRepository
+
+// sut = system under test
+let sut: SearchGymsUseCase
+
+describe('search gyms use case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+
+    sut = new SearchGymsUseCase(gymsRepository)
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to search for gyms', async () => {
+    await gymsRepository.create({
+      id: 'gym-id-1',
+      title: 'JavaScript Gym',
+      phone: '+5511999999999',
+      description: 'JavaScript Gym description',
+      latitude: -30.0028912,
+      longitude: -51.1169292,
+    })
+
+    await gymsRepository.create({
+      id: 'gym-id-1',
+      title: 'TypeScript Gym',
+      phone: '+5511999999999',
+      description: 'TypeScript Gym description',
+      latitude: -30.0028912,
+      longitude: -51.1169292,
+    })
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'JavaScript Gym' })])
+  })
+
+  it('should be able to fetch paginated gyms search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        id: `gym-id-${i}`,
+        title: `JS Gym ${i}`,
+        phone: '+5511999999999',
+        description: `JS Gym description ${i}`,
+        latitude: -30.0028912,
+        longitude: -51.1169292,
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'JS Gym',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'JS Gym 21' }),
+      expect.objectContaining({ title: 'JS Gym 22' }),
+    ])
+  })
+})
